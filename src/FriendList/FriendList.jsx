10d8547b2beae68dbdcdc3bee57e97207bab607d
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-02-11 00:38:44 +0200

    added FriendsList

--- src/FriendList/FriendList.jsx
+++ src/FriendList/FriendList.jsx
@@ -1,13 +1,14 @@
+import css from '../FriendList/FriendList.module.css';
 import FriendListItem from './FriendsListItem';
 
 function FriendList({ friends }) {
   return (
-    <ul>
-      <li>
-        {friends.map((item) => (
-          <FriendListItem items={item} key={item.id} />
-        ))}
-      </li>
+    <ul className={css.friendList}>
+      {friends.map((item) => (
+        <li key={item.id} className={css.item}>
+          <FriendListItem items={item} />
+        </li>
+      ))}
     </ul>
   );
 }
--- src/FriendList/FriendsListItem.jsx
+++ src/FriendList/FriendsListItem.jsx
@@ -1,10 +1,16 @@
 import css from '../FriendList/FriendList.module.css';
+import clsx from 'clsx';
+
 function FriendListItem({ items }) {
   return (
-    <div className={css}>
-      <img src={items.avatar} alt="Avatar" width="48" />
-      <p>{items.name}</p>
-      <p>{items.isOnline ? 'Online' : 'Offline'}</p>
+    <div className={css.friendCard}>
+      <img src={items.avatar} alt="Avatar" width="48" className={css.avatar} />
+      <p className={css.name}>{items.name}</p>
+      <p
+        className={clsx(css.status, items.isOnline ? css.online : css.offline)}
+      >
+        {items.isOnline ? 'Online' : 'Offline'}
+      </p>
     </div>
   );
 }
